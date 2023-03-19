438e22d2abf88d12b39e9c3b1c8b86a0d991bc61
Doyeon Lee <dlehdus1433@gmail.com>
2023-03-19 19:01:43 +0900

    [level 0] Title: 순서쌍의 개수, Time: 4.71 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let arr = [];
+    for (let i = 0; i <= n; i++) {
+        if (n % i === 0) {
+            arr.push(i)
+        }
+    }
+    // console.log(arr)
+    return arr.length;
+}
