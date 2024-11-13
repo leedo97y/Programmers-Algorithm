15389b764bdbe4cf4b4deec3bb9ffb8a98fe4b51
Doyeon Lee <dlehdus1433@gmail.com>
2024-11-13 23:48:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 33.93 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,14 +1,25 @@
 function solution(participant, completion) {
     const map = new Map();
     
-    for (let p of participant) {
+    for (let i = 0; i < participant.length; i++) {
+        let p = participant[i];
+        let c = completion[i];
+        
+        
         map.set(p, (map.get(p) || 0) + 1);
+        map.set(c, (map.get(c) || 0) - 1);
+        
     }
     
+// 위를 아래와 같이 대체 가능   
+//     for (let p of participant) {
+//         map.set(p, (map.get(p) || 0) + 1);
+//     }
     
-    for (let c of completion) {
-        map.set(c, (map.get(c) - 1));
-    }
+    
+//     for (let c of completion) {
+//         map.set(c, map.get(c) - 1);
+//     }
    
     for (let [key, value] of map) {
         if (value > 0) {
